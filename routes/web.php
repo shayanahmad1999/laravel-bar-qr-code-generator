a925e5743ab836dd71e922627b926baf9cc1b27a
= <=>
2025-08-04 19:21:31 +0500

    routes are added for QR and Bar code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\BarcodeController;
+use App\Http\Controllers\QRCodeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('barcode')->controller(BarcodeController::class)->group(function () {
+    Route::get('/', 'show')->name('barcode.show');
+    Route::get('/save', 'save')->name('barcode.save');
+    Route::get('/blade', 'blade')->name('barcode.blade');
+});
+
+Route::prefix('qr-code')->controller(QRCodeController::class)->group(function () {
+    Route::get('/', 'index')->name('qr.code');
+    Route::get('/save', 'SaveQRCode')->name('qr.code.save');
+    Route::get('/color', 'QRCodeCustomBackgroundColor')->name('qr.code.color');
+    Route::get('/image', 'QRCodeEmbeddedImage')->name('qr.code.image');
+    Route::get('/email', 'QRCodeEmail')->name('qr.code.email');
+    Route::get('/phone', 'QRCodePhoneNumber')->name('qr.code.phone');
+    Route::get('/sms', 'QRCodeSMS')->name('qr.code.sms');
+    Route::get('/blade', 'QRCodeBladeTemplate')->name('qr.code.blade');
 });
